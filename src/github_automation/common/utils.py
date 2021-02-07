eaa07463a779068d1a8eb37eb9dae3ab7121797d
Dean Arbel <darbel@paloaltonetworks.com>
2021-02-07 22:36:52 +0200

    fixed an issue when getting project cards

--- src/github_automation/common/utils.py
+++ src/github_automation/common/utils.py
@@ -88,6 +88,6 @@ def get_project_cards(project):
             columns = project['columns']
             if 'nodes' in columns and isinstance(columns['nodes'], list):
                 nodes = columns['nodes']
-                if len(nodes > 0) and 'cards' in nodes[0]:
+                if len(nodes) > 0 and 'cards' in nodes[0]:
                     return nodes[0]
     return {}
